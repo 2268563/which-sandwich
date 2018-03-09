8deb1c6a18c3dad31859f7cc4741bb2f7fe6470f
Gordon Wells <2268563w@student.gla.ac.uk>
2018-03-09 20:22:49 +0000

    Add first models and update admin interface

--- which_sandwich_project/whichsandwich/admin.py
+++ which_sandwich_project/whichsandwich/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from whichsandwich.models import User,Sandwich,Comment,Ingredient
 
 # Register your models here.
+
+admin.site.register(User)
+admin.site.register(Sandwich)
+admin.site.register(Comment)
+admin.site.register(Ingredient)
--- which_sandwich_project/whichsandwich/models.py
+++ which_sandwich_project/whichsandwich/models.py
@@ -1,3 +1,35 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    username = models.CharField(max_length=128, unique=True)
+    email = models.EmailField(max_length=254, unique=True)
+
+    def __str__(self):
+        return self.username
+
+class Sandwich(models.Model):
+    creator = models.ForeignKey(User, on_delete=models.PROTECT)
+    name = models.CharField(max_length=254, unique=True)
+    image = models.ImageField()
+    ingredients = models.ManyToManyField('Ingredient')
+    comments = models.ManyToManyField('Comment')
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = "Sandwiches"
+
+class Ingredient(models.Model):
+    name = models.CharField(max_length=128, unique=True)
+    calories = models.PositiveIntegerField()
+
+    def __str__(self):
+        return self.name
+
+class Comment(models.Model):
+    comment = models.TextField()
+    user = models.ForeignKey(User, on_delete=models.PROTECT)
+
+    def __str__(self):
+        return self.comment
