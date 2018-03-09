0d903b49aef8c95291937679e93632946bf13de2
Gordon Wells <2268563w@student.gla.ac.uk>
2018-03-09 23:44:59 +0000

    Add user favourites and sandwich rating to models
    Likes and dislikes on a sandwich are stored as integers in separate
    rows in the database.

--- which_sandwich_project/whichsandwich/models.py
+++ which_sandwich_project/whichsandwich/models.py
@@ -3,6 +3,7 @@ from django.db import models
 class User(models.Model):
     username = models.CharField(max_length=128, unique=True)
     email = models.EmailField(max_length=254, unique=True)
+    favourites = models.ManyToManyField('Sandwich', blank=True)
 
     def __str__(self):
         return self.username
@@ -12,6 +13,8 @@ class Sandwich(models.Model):
     name = models.CharField(max_length=256, unique=True)
     image = models.ImageField(blank=True)
     ingredients = models.ManyToManyField('Ingredient')
+    likes = models.PositiveIntegerField(blank=True, default=0)
+    dislikes = models.PositiveIntegerField(blank=True, default=0)
 
     def __str__(self):
         return self.name
