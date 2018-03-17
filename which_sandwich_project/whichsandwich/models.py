02fcd1212c7d3ed2c5fcccd6a766456c2db362f0
Gordon Wells <2268563w@student.gla.ac.uk>
2018-03-17 23:39:57 +0000

    Add slug to sandwich model

--- which_sandwich_project/whichsandwich/models.py
+++ which_sandwich_project/whichsandwich/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
+from django.utils.text import slugify
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
@@ -26,12 +27,17 @@ class Sandwich(models.Model):
     ingredients = models.ManyToManyField('Ingredient')
     likes = models.PositiveIntegerField(blank=True, default=0)
     dislikes = models.PositiveIntegerField(blank=True, default=0)
+    slug = models.SlugField(unique=True)
 
     def __str__(self):
         return self.name
 
     class Meta:
         verbose_name_plural = "Sandwiches"
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        super().save(*args, **kwargs)
 
 class Ingredient(models.Model):
     name = models.CharField(max_length=128, unique=True)
