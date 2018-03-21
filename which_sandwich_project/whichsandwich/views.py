75d35121f9dc9fb3ffe4d982827b45b41bc01528
Gordon Wells <2268563w@student.gla.ac.uk>
2018-03-21 18:12:56 +0000

    Fix the sign_up view
    Users are now created properly and redirected to the home page.

--- which_sandwich_project/whichsandwich/views.py
+++ which_sandwich_project/whichsandwich/views.py
@@ -83,35 +83,26 @@ def sign_up(request):
     # If it's a HTTP POST, we're interested in processing form data.
     if request.method == 'POST':
         # Attempt to grab information from the raw form information.
-        # Note that we make use of both UserForm and UserProfileForm.
         user_form = UserForm(data=request.POST)
-        profile_form = UserProfileForm(data=request.POST)
         
         # If the two forms are valid...
-        if user_form.is_valid() and profile_form.is_valid():
-            # Save the user's form data to the database.
-            user = user_form.save()
+        if user_form.is_valid():
+            # Create the user instance
+            user = user_form.save(commit=False)
             
             # Now we hash the password with the set_password method.
-            # Once hashed, we can update the user object.
             user.set_password(user.password)
-            user.save()
             
-            # Now sort out the UserProfile instance.
-            # Since we need to set the user attribute ourselves,
-            # we set commit=False. This delays saving the model
-            # until we're ready to avoid integrity problems.
-            profile = profile_form.save(commit=False)
-            profile.user = user
+            # User profile is created automatically
             
             # Did the user provide a profile picture?
             # If so, we need to get it from the input form and
-            #put it in the UserProfile model.
+            # put it in the UserProfile model.
             if 'picture' in request.FILES:
-                profile.picture = request.FILES['picture']
+                user.profile.picture = request.FILES['picture']
                 
             # Now we save the UserProfile model instance.
-            profile.save()
+            user.save()
             
             # Update our variable to indicate that the template
             # registration was successful.
@@ -119,18 +110,16 @@ def sign_up(request):
         else:
             # Invalid form or forms - mistakes or something else?
             # Print problems to the terminal.
-            print(user_form.errors, profile_form.errors)
+            print(user_form.errors)
     else:
         # Not a HTTP POST, so we render our form using two ModelForm instances.
         # These forms will be blank, ready for user input.
         user_form = UserForm()
-        profile_form = UserProfileForm()
         
     # Render the template depending on the context.
     return render(request,
                   'whichsandwich/sign_up.html',
                   {'user_form': user_form,
-                   'profile_form': profile_form,
                    'registered': registered})
 
 def sign_in(request):
