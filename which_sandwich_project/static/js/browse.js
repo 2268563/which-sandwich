f1cf0919ff1bf7f73645dcde83529571c5157198
Gordon Wells <2268563w@student.gla.ac.uk>
2018-03-27 20:13:06 +0100

    Improve sandwich modal
    Information such as creator, ingredients and a random comment are now
    included on the modal.

--- which_sandwich_project/static/js/browse.js
+++ which_sandwich_project/static/js/browse.js
@@ -6,7 +6,12 @@ $("document").ready(function() {
     });
 
     $("#sandwich_display").click(function(e) {
-        var sandwich_id = e.target.closest('div[class^="sandwich"]').id;
+        sandwich_container = e.target.closest('div[class^="sandwich"]');
+        sandwich_id = sandwich_container.id;
+        sandwich_name = $(sandwich_container).attr("name");
+
+        // Set modal title to sandwich name
+        $("#sandwich-modal-title").text(sandwich_name);
         getSandwichModal(sandwich_id);
         $("#sandwichModal").modal("toggle");
     });
@@ -20,6 +25,7 @@ function filterSandwiches(filter){
 }
 
 function getSandwichModal(sandwich_id) {
+    console.log(sandwich_id);
     $.get('/whichsandwich/sandwich_modal/', { sandwich_id: sandwich_id, }, function(data) {
         $('.modal-body').html(data);
     });
